e3aa336be526ccf73e386de94ae383936d5af87f
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2026-01-03 18:06:31 +0700

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from api.index import home_, api_
+    from api.index import home_, api_, uptime, uptime_web
     app.register_blueprint(home_)
     app.register_blueprint(api_)
+    app.register_blueprint(uptime)
+    app.register_blueprint(uptime_web)
 
     return app
