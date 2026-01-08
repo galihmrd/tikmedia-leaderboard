66bdea7a3936c4cd365bf87da2c84a31bc7ec76a
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2026-01-08 11:57:08 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
 import os
 from datetime import datetime
 import requests
-from flask import Blueprint, render_template, jsonify
-from api.db import get_top_rankings
+from flask import Blueprint, render_template, jsonify, request
+from api.db import get_top_rankings, get_user_ranking
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -65,8 +65,27 @@ api_ = Blueprint("api", __name__)
 @api_.route('/api')
 def home():
     data_list = []
-    res = get_top_rankings()
-    for data in res:
+    id_user = request.args.get("id")
+    if not id_user:
+        res = get_top_rankings()
+        for data in res:
+            user = requests.get(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMember?chat_id={data['user_id']}&user_id={data['user_id']}"
+            ).json()
+            tele_user = "Telegram User"
+            if user["ok"]:
+                tele_user = user["result"]["user"]["first_name"]
+            data_list.append(
+                {
+                    "rank": data["rank"],
+                    "user_id": data["user_id"],
+                    "name": tele_user,
+                    "timestamp": data["last_updated"],
+                    "downloads": data["total_actions"]
+                }
+            )
+    else:
+        data = get_user_ranking(id_user)
         user = requests.get(
             f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMember?chat_id={data['user_id']}&user_id={data['user_id']}"
         ).json()
@@ -74,12 +93,12 @@ def home():
         if user["ok"]:
             tele_user = user["result"]["user"]["first_name"]
         data_list.append(
-            {
-                "rank": data["rank"],
-                "user_id": data["user_id"],
-                "name": tele_user,
-                "timestamp": data["last_updated"],
-                "downloads": data["total_actions"]
-            }
+                {
+                    "rank": data["rank"],
+                    "user_id": data["user_id"],
+                    "name": tele_user,
+                    "timestamp": data["last_updated"],
+                    "downloads": data["total_actions"]
+                }
         )
     return jsonify({"users": data_list})
