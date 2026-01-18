7383d424f30947c7cabc8c5bb75fd9e620787b18
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2026-01-18 23:06:57 +0700

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from api.index import home_, api_, uptime, downloader_web, dl_api, is_tikmedia_member, tikmedia_unblock, uptime_web
+    from api.index import home_, api_, uptime, downloader_web, dl_api, is_tikmedia_member, tikmedia_unblock, tikmedia_unblock_web, uptime_web
     app.register_blueprint(home_)
     app.register_blueprint(api_)
     app.register_blueprint(uptime)
@@ -12,5 +12,6 @@ def create_app():
     app.register_blueprint(dl_api)
     app.register_blueprint(is_tikmedia_member)
     app.register_blueprint(tikmedia_unblock)
+    app.register_blueprint(tikmedia_unblock_web)
 
     return app
